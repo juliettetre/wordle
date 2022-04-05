cbdb70cc8fc46cd9e5e748978202d037691a1883
ENSTA <valentin.ellerbach@ensta_paris.fr>
2022-04-05 18:05:00 +0200

    nouveaaau

--- jeu.c
+++ jeu.c
@@ -4,87 +4,31 @@
 #include <stdbool.h>
 #include <time.h>
 
-
-struct word_list_t 
+/*
+Cette fontion ouvre le fichier et renvois le tableau de mot associé
+*/
+char **load_file (char *fname, unsigned int size)
 {
-  char *word ;
-  struct word_list_t *next ;
-};
+    char buffer[32];
+    char **words_array = malloc(sizeof(char*)*size);
+    FILE *f ;
 
-char **load_file (char *fname, unsigned int *size)
-{
-  char **words_array ;
-  unsigned int count_words = 0 ;
-  struct word_list_t *wl = NULL ;
-  char buffer[256] ;
-  FILE *in_hd ;
+    f = fopen (fname, "rb") ;
+    if (f== NULL) return (NULL) ;
 
-  /* First open the file and check success. */
-  in_hd = fopen (fname, "rb") ;
-  if (in_hd == NULL) return (NULL) ;
-
-  /* Read the first word. */
-  fscanf (in_hd, "%s", buffer) ;
-  while (! feof (in_hd)) 
-  {
-    char *word_cpy ;
-    struct word_list_t *new_cell = malloc (sizeof (struct word_list_t*)) ;
-    if (new_cell == NULL) 
+    fscanf (f, "%s", buffer) ;
+    int indice=0;
+    while (! feof (f)) 
     {
-      /* Close the opened file. */
-      fclose (in_hd) ;
-      /* BAD: We should also free the already allocated string list cells, but
-         I'm too lazzy ^^ Don't do this at home... */
-      return (NULL) ;
+        words_array[indice] = malloc(sizeof(buffer));
+        strcpy(words_array[indice],buffer);
+        fscanf (f, "%s", buffer) ;
+        indice++;
     }
-    /* Allocate memory to store the word. */
-    word_cpy = malloc ((1 + strlen (buffer)) * sizeof (char)) ;
-    if (word_cpy == NULL) 
-    {
-      /* BAD: Same comment than above. */
-      fclose (in_hd) ;
-      return (NULL) ;
-    }
-    /* Copy the buffer into the fresh string. */
-    strcpy (word_cpy, buffer) ;
-    new_cell->word = word_cpy ;
-    /* Now, link the cell. */
-    new_cell->next = wl ;
-    wl = new_cell ;
-    count_words++ ;
-
-    /* Read the next word. */
-    fscanf (in_hd, "%s", buffer) ;
-  }
-
-  /* Now, close the file since we do not need it anymore. */
-  fclose (in_hd) ;
-
-  /* It's now time to transfer the string into an array. Attention, the list
-     construction has reversed the order of the words. */
-  words_array = malloc (count_words * sizeof (char**)) ;
-  if (words_array == NULL) {
-    /* BAD: We should free the already allocated memory. */
-    return (NULL) ;
-  }
-
-  /* Store the size now since we will use 'count_words' to fill the array. */
-  *size = count_words ;
-
-  while (count_words != 0) {
-    /* Attention: by invariant, wl->next is never NULL while in the loop. This
-       is due to the fact that the length of the list is exactly
-       'count_words'. */
-    struct word_list_t *tmp = wl->next ;
-    count_words-- ;
-    /* Copy the pointer hence share the string. So, do not free it now ! */
-    words_array[count_words] = wl->word ;
-    /* Go to the next cell of the list. */
-    wl = tmp ;
-  }
-
-  return (words_array) ;
+    return(words_array);
 }
+
+
 
 
 
@@ -144,7 +88,7 @@ void get_word(char** dico,unsigned int size_dico, char* current_word)
     while (! stop)
     {   
         printf("entrez un mot de 5 lettres svp\n");
-        char* p = scanf(" %s",buffer);
+        scanf(" %s",buffer);
         if (strlen(buffer)!=5)
         {
             printf("le mot ne fait pas 5 lettres\n");
@@ -228,10 +172,10 @@ void  analyse(char* current_word, char* secret_word)
 
 
 int main(int argc, char* argv[])
-{
+{ 
     srand(time(0));
-    unsigned int size_dico ;
-    char** dico = load_file("dico_5.txt",&size_dico);
+    unsigned int size_dico = 7645 ;
+    char** dico = load_file("dico_5.txt",size_dico);
     char word[16];
     char* secret_word = word_select(dico,size_dico,word);
     printf("%s\n",secret_word);
