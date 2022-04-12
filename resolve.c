21c47094639b4f2bce9d9a39b6a3953b32ff75e4
ENSTA <valentin.ellerbach@ensta_paris.fr>
2022-04-12 16:53:13 +0200

    TD3

--- resolve.c
+++ resolve.c
@@ -2,63 +2,81 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <math.h>
 
 
 /*
-HONTEUX... à modifier .........
+Cette fontion ouvre le fichier et renvois le tableau de mot associé
 */
-int** get_config_array(int N)
+char **load_file (char *fname, unsigned int size)
 {
-    int** config_array = malloc(sizeof(int*)*243);
-    int c=0;
-    for(int i=0;i<3;i++)
+    char buffer[32];
+    char **words_array = malloc(sizeof(char*)*size);
+    FILE *f ;
+
+    f = fopen (fname, "rb") ;
+    if (f== NULL) return (NULL) ;
+
+    fscanf (f, "%s", buffer) ;
+    int indice=0;
+    while (! feof (f)) 
     {
-        for(int j=0;j<3;j++)
+        words_array[indice] = malloc(sizeof(buffer));
+        strcpy(words_array[indice],buffer);
+        fscanf (f, "%s", buffer) ;
+        indice++;
+    }
+    return(words_array);
+}
+
+/*
+Renvoie la liste des char correspondant aux config possibles et prend en arguments la taille du mot
+*/
+char** get_config_array(int N)
+{
+    int n = pow(3,N);
+    char** config_array = malloc(sizeof(char*)*n);
+    char* config;
+    for(int i=0;i<n;i++)
+    {
+        config = malloc(N*sizeof(char));
+        int I = i;
+        for(int e=0;e<N;e++)
         {
-            for(int k=0; k<3;k++)
+            int r = I%3;
+            I=I/3;
+            if(r==0)
             {
-                for(int l=0; l<3;l++)
-                {
-                    for(int m = 0; m<3 ; m++)
-                    {
-                        int* L = malloc(sizeof(int)*N);
-                        L[0]=i;
-                        L[1]=j;
-                        L[2]=k;
-                        L[3]=l;
-                        L[4]=m;
-                        //printf("%d %d %d %d %d \n",i,j,k,l,m);
-                        config_array[c] = malloc(sizeof(L));
-                        config_array[c] = L;
-                        c++;
-                    }
-                }
+                config[e] = '0';
+            }
+            if(r==1)
+            {
+                config[e] = '1';
+            }
+            if(r==2)
+            {
+                config[e] = '2';
             }
         }
+        config_array[i] = malloc(sizeof(config));
+        config_array[i] = config;
     }
-    //printf("%d \n",c);
     return(config_array);
 }
 
-
 /*
-afficher un double tableau d'entier
+Afficher un double tableau d'entier
 */
-void super_print(int** tab)
+void super_print(char** tab, int N)
 {
-    for(int i=0;i<243;i++)
+    for(int i=0;i<pow(3,N);i++)
     {
-        for(int j=0;j<5;j++)
-        {
-            printf("%d",tab[i][j]);
-        }
-        printf("\n");
+        printf("%s\n",tab[i]);
     }
-    printf("\n");
 }
 
 /*
-renvois en entier corespondant au nombre d'occurence de letter dans word
+Renvoie en entier corespondant au nombre d'occurence de letter dans word
 */
 int occurences (char* word, char letter)
 {
@@ -74,9 +92,9 @@ int occurences (char* word, char letter)
 }
 
 /*
-Revois true si le mot est compatible avec la config et false sinon
+Revoie true si le mot est compatible avec la config et false sinon
 */
-bool is_compatible(char* word_ref, int* config, char* word_test)
+bool is_compatible(char* word_ref, char* word_test, char* config)
 {
     bool stop=false;
     char* list_precense = calloc(5,sizeof(char));
@@ -85,7 +103,7 @@ bool is_compatible(char* word_ref, int* config, char* word_test)
 // on test les vert (2)
     for(int i=0; i<5;i++)
     {
-        if(config[i]==2)
+        if(config[i]=='2')
         {
             if(word_ref[i]==word_test[i])
             {
@@ -103,7 +121,7 @@ bool is_compatible(char* word_ref, int* config, char* word_test)
     for(int i=0;i<5;i++)
     {
         stop = true;
-        if(config[i]==1)
+        if(config[i]=='1')
         {
             stop = false;
             for (int j=0;j<5;j++)
@@ -140,13 +158,20 @@ bool is_compatible(char* word_ref, int* config, char* word_test)
     //on test les gris (0)
     for(int i=0;i<5;i++)
     {
-        if (config[i]==0)
+        if (config[i]=='0')
         {
             for (int j=0;j<5;j++)
             {
                 if(word_ref[i]==word_test[j])
                 {
-                    if(occurences(word_ref,word_ref[i])<=occurences(list_precense,word_ref[i]))
+                    if (strlen(list_precense)>0)
+                    {
+                        if(occurences(word_ref,word_ref[i])<=occurences(list_precense,word_ref[i]))
+                        {
+                            return(false);
+                        }
+                    }
+                    else
                     {
                         return(false);
                     }
@@ -159,38 +184,89 @@ bool is_compatible(char* word_ref, int* config, char* word_test)
 }
 
 /*
-int get_entropy(char* word, char** word_array, unsigned int size, int** config_array)
-{
-
-
-    for(int id_config = 0; id_config < 243 ; id_config++)
-    {
-
-    }
-
-}
+Renvoie le nombre de mot du dico compatible avec word_ref et la config choisie
 */
+int get_number_compatible(char* config,char* word_ref,unsigned int size_curr_dico, char** word_array)
+{
+    int count = 0;
+    for(int i=0;i<size_curr_dico;i++)
+    {
+        char* word_test = word_array[i];
+        if(is_compatible(word_ref,word_test,config))
+        {
+            count++;
+        }
+    }
+    return(count);
+}
 
+/*
+Renvoie l'entropie de word_ref
+*/
+double get_entropy(char* word_ref, char** word_array, unsigned int size_curr_dico, char** config_array, int N)
+{
+    double entropy = 0;
+    for(int i=0; i<pow(3,N) ; i++)
+    {
+        char* config = config_array[i];
+        int number_compatible = get_number_compatible(config,word_ref,size_curr_dico,word_array);
+        double number_f = number_compatible;
+        //printf("%s donne : %f \n",config,number_f);
+        double size_f = size_curr_dico;
+        double p = number_f/size_f;
+        double P = 0;
+        if(p!=0)
+        {
+            //printf("%s donne : %f\n",config,p);
+            P = (-p)*(log2(p));
+            entropy+=P;
+        }
+        //printf("%s donne proba : %f et entropie : %f\n",config,p,P);
+    }
+    return(entropy);
+}
 
+char* get_best_word(char** word_array, char** config_array, int N, unsigned int size_curr_dico)
+{
+    char* best_word = word_array[0];
+    double best_entropy = get_entropy(best_word,word_array,size_curr_dico,config_array,N);
+    for(int i=0;i<size_curr_dico;i++)
+    {
+        if(i%10 == 0)
+        {
+            double ii = i;
+            printf("%f pourcent\n",(7.7*ii)/100);
+        }
+        char* curr_word = word_array[i];
+        double curr_entropy = get_entropy(curr_word,word_array,size_curr_dico,config_array,N);
+        if(curr_entropy>best_entropy)
+        {
+            best_entropy=curr_entropy;
+            best_word = curr_word;
+        }
+    }
+    return(best_word);
+}
 
-
-
-
-
-
-
-
-
-
+void free_tab_char(char** tab,int size_tab)
+{
+    for(int i=0;i<size_tab;i++)
+    {
+        free(tab[i]);
+    }
+    free(tab);
+}
 
 
 int main(int argc, char* argv[])
 {
-    
-    
-    int** A = get_config_array(5);
-    super_print(A);
-    bool T = is_compatible("ZZEEE",A[4],"YTTEY");
-    printf("%d \n",T);
+    int N = 5;
+    unsigned int size_dico = 7645;
+    char** word_array = load_file("dico_5.txt",size_dico);
+    char** config_array = get_config_array(N);
+    char* best_word = get_best_word(word_array,config_array,N,size_dico);
+    printf("%s\n",best_word);
+    free_tab_char(word_array,size_dico);
+    free_tab_char(config_array,pow(3,N));
     return(0);
 }
